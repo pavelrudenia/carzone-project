cbb60553da87d6ca51bd8b37f51a979e3c2dc9be
pavelrudenia <pavelrudeniaaa@gmail.com>
2022-09-23 19:32:29 +0300

    admin Cars page

--- carzone/settings.py
+++ carzone/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
-    'cars.apps.CarsConfig'
+    'cars.apps.CarsConfig',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
